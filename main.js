7a0305aec752ad7fbf2c2004034e9195b4038da5
Valera <thedatahord@yahoo.co.jp>
2023-07-01 11:07:42 +0300

    Add ver 1.0

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // Включаем nodeIntegration
+      webviewTag: true // Включаем поддержку тега webview
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+
+  // Опционально: Открытие инструментов разработчика
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
