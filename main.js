03a97b3b94ee2ffca8902c510bc3aca7651dfa40
Valera <thedatahord@yahoo.co.jp>
2023-07-01 12:12:33 +0300

    Add ver 1.1

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1080,
+    height: 720,
     webPreferences: {
       nodeIntegration: true, // Включаем nodeIntegration
       webviewTag: true // Включаем поддержку тега webview
